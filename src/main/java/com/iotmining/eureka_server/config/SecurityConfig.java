91e1e30460a6d48e73465b873ff354f700568f24
S-8506 <s.8506.a@gmail.com>
2025-06-08 13:44:51 +0530

    Add security to eureka-server.

---
+++ src/main/java/com/iotmining/eureka_server/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.iotmining.eureka_server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                )
+                .formLogin(withDefaults())  // ← this enables the default Spring login form
+                .logout(LogoutConfigurer::permitAll)
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+}
